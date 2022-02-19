465af626fef480d9af7b8ded14b2b109af7abf5d
Agustin Mendez <mendezagustin94@gmail.com>
2022-02-19 20:33:50 -0300

    para luchito

---
+++ routes/admin/login.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var conocido = Boolean(req.session.nombre);
+  
+  res.render('admin/login',{
+    layout:'admin/layout',
+    conocido: conocido,
+    nombre: req.session.nombre
+});
+});
+
+module.exports = router;
